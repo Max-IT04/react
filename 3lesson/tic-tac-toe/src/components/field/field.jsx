4f0a312f2d7675f13cd5f14606f6c617ac009dc9
Maxim Roschin <maks.roshin2004@gmail.com>
2026-03-01 23:21:14 +0300

    Добавил проект из 3 урока Крестики-Нолики

---
+++ 3lesson/tic-tac-toe/src/components/field/field.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import { PLAYER } from '../../constants';
+import { FieldLayout } from './field-layout';
+
+export const Field = ({ field, handleCellClick }) => {
+  return <FieldLayout field={field} handleCellClick={handleCellClick} />;
+};
+
+Field.propTypes = {
+  field: PropTypes.arrayOf(
+    PropTypes.oneOf([PLAYER.CROSS, PLAYER.NOUGHT, PLAYER.NOBODY]),
+  ),
+  handleCellClick: PropTypes.func, 
+};
---
+++ 3lesson/tic-tac-toe/src/game.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { GameLayout } from './game-layout';
+import { handleCellClick, handleRestart } from './handlers';
+import { createEmptyField } from './utils';
+import { STATUS, PLAYER } from './constants';
+
+export const Game = () => {
+  const [status, setStatus] = useState(STATUS.TURN);
+  const [currentPlayer, setCurrentPlayer] = useState(PLAYER.CROSS);
+  const [field, setField] = useState(createEmptyField());
+
+  const state = { status, setStatus, currentPlayer, setCurrentPlayer, field, setField };
+
+  return ( 
+    <GameLayout 
+      status={status} 
+      currentPlayer={currentPlayer} 
+      field={field} 
+      handleCellClick={(cellIndex) => handleCellClick(state, cellIndex)} 
+      handleRestart={() => handleRestart(state)}
+    /> 
+  );
+};
